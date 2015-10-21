8222a2988695faf3c49e1fed60ebbda38f6f3909
zlozano7546 <zlozano7546@001LAB153010823.neisd.ad>
2015-10-21 15:23:09 -0500

    Intial commit

---
+++ StarWars/src/TheGreenGuy.java
@@ -0,0 +1,11 @@
+
+public class TheGreenGuy 
+{
+	public static void main (String args[])
+	{
+System.out.println("THE JAVA IS WITH THIS ONE AND CRAP");
+System.out.println("I FORGOT WHAT I WAS SUPPOSED TO PUT IN THIS");
+System.out.println("Bow chicka bow wow");
+System.out.println("20 Chromosones");
+}
+}
